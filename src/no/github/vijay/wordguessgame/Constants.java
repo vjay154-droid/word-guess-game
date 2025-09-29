c19ad8f16dd11e3818b105557ac907c54d31bf3e
SHIVIJ <vijay.shivappa@tryg.no>
2025-09-29 17:43:27 +0200

    Refactor: Add Constants.java for WORD_LENGTH and MAX_ATTEMPTS

---
+++ src/no/github/vijay/wordguessgame/Constants.java
@@ -0,0 +1,10 @@
+package no.github.vijay.wordguessgame;
+
+public class Constants {
+	public static final int WORD_LENGTH = 5;
+	public static final int MAX_GUESS = 5;
+	
+	private Constants(){
+		
+	}
+}
